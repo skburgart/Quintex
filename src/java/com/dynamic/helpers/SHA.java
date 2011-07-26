c3fe9e8b9e9874afa155a5ce59d8902ab404a833
Steven Burgart <skb08c@my.fsu.edu>
2011-07-25 20:49:27 -0400

    Login servlet & helper classes

---
+++ src/java/com/dynamic/helpers/SHA.java
@@ -0,0 +1,48 @@
+/**
+ * Created By Tathagata Mukherjee
+ * Jun 5, 2011
+ * Modified by Steven Burgart
+ * Jul 21, 2011
+ * 2011
+ * Player
+ */
+package com.dynamic.helpers;
+
+import java.security.MessageDigest;
+import java.util.Formatter;
+
+public class SHA {
+
+    public static String getSHAOne(String toEncode) {
+        MessageDigest md = null;
+
+        try {
+            md = MessageDigest.getInstance("SHA-1");
+        } catch (Exception exp) {
+            Utility.logError(exp);
+        }
+
+        return byteToBase16(md.digest(toEncode.getBytes()));
+    }
+
+    public static String getSHA256(String toEncode) {
+        MessageDigest md = null;
+
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (Exception exp) {
+            Utility.logError(exp);
+        }
+
+        return byteToBase16(md.digest(toEncode.getBytes()));
+    }
+
+    private static String byteToBase16(byte[] hash) {
+        Formatter formatter = new Formatter();
+        for (byte b : hash) {
+            formatter.format("%02x", b);
+        }
+        return formatter.toString();
+    }
+    
+}
---
+++ src/java/com/dynamic/helpers/Utility.java
@@ -0,0 +1,31 @@
+/**
+ * @author steve
+ */
+package com.dynamic.helpers;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Utility {
+
+    public static void log(String message) {
+        boolean debugging = true;
+
+        if (debugging) {
+            Calendar cal = Calendar.getInstance();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss z");
+            String dateTime = sdf.format(cal.getTime());
+            System.out.println("[" + dateTime + "] " + message);
+        }
+    }
+
+    public static void logError(Exception exp) {
+        boolean stackTrace = true;
+
+        log("ERROR: " + exp.getMessage());
+
+        if (stackTrace) {
+            exp.printStackTrace();
+        }
+    }
+}
