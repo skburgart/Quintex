f8757b988f66d13be3507305ddaf6b4fd948157f
Steven Burgart <skb08c@my.fsu.edu>
2011-08-05 22:12:48 -0400

    Basic post-topic framework

---
+++ web/js/ajax/post-topic.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+    $('input[name=topic-title]').focus();
+})
+
+function register() {
+    var url = "register";
+    var data = {
+        "username": $('input[name=topic-title]'),
+        "email": $('input[name=topic-message]'),
+        "password1": $('input[name=register-password1]'),
+        "password2": $('input[name=register-password2]')
+    }
+
+    if (validateRegistration(data)) {
+        $('.register-input').attr("disabled", "true");
+        ajaxRequest(url, {
+            "username": data.username.val(),
+            "email": data.email.val(),
+            "password": data.password1.val()
+        }, parseRegistration)
+    }
+}
+
+function validateRegistration(data) {
+    var valid = false;
+
+    if (!data.username.val()) {
+        registerMessage("Enter username");
+        data.username.focus();
+    } else if (data.username.val().length < 3) {
+        registerMessage("Username must be at least 3 characters");
+        data.username.focus();
+    } else if (data.username.val().length > 32) {
+        registerMessage("Username must be at most 32 characters");
+        data.username.focus();
+    } else if (!validateUsername(data.username.val())) {
+        registerMessage("Username must start with a letter and not contain special characters");
+        data.username.focus();
+    } else if (!validateEmail(data.email.val())) {
+        registerMessage("Invalid email address");
+        data.email.focus();
+    } else if (!data.password1.val()) {
+        registerMessage("Enter password");
+        data.password1.focus();
+    } else if (!data.password2.val()) {
+        registerMessage("Enter password confirmation");
+        data.password2.focus();
+    } else if (data.password1.val() != data.password2.val()) {
+        registerMessage("Passwords do not match");
+        data.password1.focus();
+    } else if (data.password1.val().length < 6){
+        registerMessage("Password must be at least 6 characters");
+        data.password1.focus();
+    } else {
+        valid = true;
+    }
+
+    return valid;
+}
+
+function parseRegistration (xmlResponse) {
+    var response = $(xmlResponse).find("registerResponse").text();
+
+    if (response == 1) {
+        registerMessage("Success! You may now log in");
+    } else {
+        $('.register-input').removeAttr('disabled');
+        if (response == 2) {
+            registerMessage("That username already exists. Please choose another.");
+            $('input[name=register-username]').val("").focus();
+        } else {
+            registerMessage("Registration failed");
+        }
+    }
+
+}
+
+function registerMessage(message) {
+    $('#register-msg').text(message);
+    $('#register-msg').fadeIn("fast");
+    $('#register-msg').effect("highlight", {}, 1000);
+}
--- web/user/new-topic.jsp
+++ web/user/new-topic.jsp
@@ -4,18 +4,36 @@
     Author     : steve
 --%>
 
+<%@page import="com.quintex.value.BoardVO"%>
+<%@page import="com.quintex.objects.BoardDBO"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
-<html>
+<html>   
+    <%
+        BoardDBO bdbo = new BoardDBO();
+
+        int boardid = Integer.parseInt(request.getParameter("boardid"));
+
+        BoardVO thisBoard = bdbo.get(boardid);
+    %>
     <head>
         <title>Quintex</title>
         <jsp:include page="/common-headers.jsp"/>
+        <script src="/Quintex/js/form-validation.js"></script>
+        <script src="/Quintex/js/ajax/post-topic.js"></script>
     </head>
     <body>
         <jsp:include page="header.jsp"/>
         <div id='content'>
-            <h2>Create a Topic</h2>
-            <p>*create topic here*</p>
+            <form action="javascript:newTopic()">
+                <h2 class="center"><%= thisBoard.getTitle()%></h2>
+                <h3>Create New Topic</h3>
+                <p>Topic Title</p>
+                <p><input name="topic-title" id="new-topic-input" type="text" /></p>
+                <p>Message</p>
+                <p><textarea name="topic-message" id="message-textarea" cols="50" rows="10" ></textarea></p>
+                <p><input class="submit" type="submit" value="Post Topic" />
+            </form>
         </div>
         <jsp:include page="/footer.jsp"/>
     </body>
