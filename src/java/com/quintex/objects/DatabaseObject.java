513bd456395753ffacf8277fb4f69d3e509a686c
Steven Burgart <skb08c@my.fsu.edu>
2012-06-22 20:41:24 -0400

    Fixes

--- src/java/com/quintex/objects/DatabaseObject.java
+++ src/java/com/quintex/objects/DatabaseObject.java
@@ -1,11 +1,7 @@
 package com.quintex.objects;
 
 import com.quintex.helpers.Logger;
-import java.sql.PreparedStatement;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  *
